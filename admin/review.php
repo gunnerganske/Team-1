46ce8c380cf2791c5aacdcac5ca681c9f272a9bc
gunnerganske <gganske15@winona.edu>
2017-11-04 16:01:22 -0500

    Avg and Sum Functions

--- admin/review.php
+++ admin/review.php
@@ -9,9 +9,9 @@
 	<?php 
 //ALL variables just examples, must be Changed when running different database
 	$servername = "localhost";
-	$dbname = "myDB";
-	$password = "password";
-	$username = "username";
+	$dbname = "imdb small";
+	$password = "Suzuki14";
+	$username = "gunnerganske";
 	
 //Creating Connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -19,12 +19,27 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if($conn->connect_error){
 	die("Connection Failure: " . $conn->connect_error);
 }
-$sql = "Insert SQL Query Here";
-$result = $conn->query($sql);
+$sqlAvg = "SELECT AVG(film_count) FROM actors";
+$resultAvg = $conn->query($sqlAvg);
+$sqlSum = "SELECT SUM(film_count) FROM actors";
+$resultSum = $conn->query($sqlSum);
 
 //Run For/Foreach/While/If Statements here
  
 //Average Function 
+if($resultAvg->num_rows > 0){
+	while($rowAvg=$resultAvg-> fetch_assoc()){
+		echo "Average of Film_Count: " . $rowAvg["AVG(film_count)"] . "<br>";
+	}
+	
+}
+
+if($resultSum->num_rows > 0){
+	while($rowSum=$resultSum-> fetch_assoc()){
+		echo "Sum of Film_Count: " . $rowSum["SUM(film_count)"] . "<br>";
+	}
+	
+}
 //Count Function
 //Name Functions
 
