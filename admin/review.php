d9ad8d9217ac932a554fa2b6117edbb08a5c6304
gunnerganske <gganske15@winona.edu>
2017-11-04 14:39:25 -0500

    Sudo Review.php

--- admin/review.php
+++ admin/review.php
@@ -5,6 +5,32 @@
 		<title> Title </title>
 	</head>
 	<body>
+	
+	<?php 
+//ALL variables just examples, must be Changed when running different database
+	$servername = "localhost";
+	$dbname = "myDB";
+	$password = "password";
+	$username = "username";
+	
+//Creating Connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//Checkgin Connection
+if($conn->connect_error){
+	die("Connection Failure: " . $conn->connect_error);
+}
+$sql = "Insert SQL Query Here";
+$result = $conn->query($sql);
+
+//Run For/Foreach/While/If Statements here
+
+
+
+
+//Closing Connection to Server
+$conn->close();
+	?>
+
 		
 	</body>
 </html>
