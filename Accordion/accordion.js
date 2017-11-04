97d030a3764a85acab18f43d9904512be52602f7
Joseph <no6133nz@no6133nzm7.workstations.winona.edu>
2017-11-04 14:57:22 -0500

    accordion

--- Accordion/accordion.js
+++ Accordion/accordion.js
@@ -1,17 +1,17 @@
 
-
+var temp = null;
 $(document).ready(function ($){
     var panels = $(".accordion > dd").hide();
-
-    
-
     $(".accordion > dt > a").click(function(){
         
         var $this = $(this);
-
         panels.slideUp();
-
-        $this.parent().next().slideDown();
+        var temp1 = $this.parent().children().html() + "";
+        if(temp == null || temp != temp1){
+        	$this.parent().next().slideDown();
+        	temp = String($this.parent().children().html() + "");
+        }
+        else{temp = null;}
         return false;
     });
 
