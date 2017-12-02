5e4404e3f29d0ab9be04b596fb3dd6d94d63db39
koogordo <ko2761oe@go.minnstate.edu>
2017-12-01 21:31:55 -0600

    deleteOLD

--- green-work-bench/php/nav.php
+++
@@ -1,9 +0,0 @@
-
-<div id="dropMenu">
-    <ul id="dropnavList">
-        <li>
-            <a href="../index.php">Logout</a>
-        </li>
-    </ul>
-</div>
-<button id="navBtn"><img src="../style/userico.png" alt="user"></button>
